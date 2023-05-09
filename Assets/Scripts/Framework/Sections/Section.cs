792515ab2b522c6a207507584514a5eb2f2188d6
beneffe <ben.gladine@fabriquefantastique.be>
2023-05-09 18:04:56 +0200

    Pickupmanager implemented. Also added TriggerHandler and GameManager

--- Assets/Scripts/Framework/Sections/Section.cs
+++ Assets/Scripts/Framework/Sections/Section.cs
@@ -3,17 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(100)]
 public class Section : MonoBehaviour
 {
     public delegate void SectionCallBack(Section section);
     public event SectionCallBack Loaded;
-    
-    /// <summary>
-    /// The list of pickups in the section
-    /// </summary>
-    private List<PickUp> _pickUps = new List<PickUp>();
 
-    private void OnEnable()
+    private void Start()
     {
         Loaded?.Invoke(this);
     }
