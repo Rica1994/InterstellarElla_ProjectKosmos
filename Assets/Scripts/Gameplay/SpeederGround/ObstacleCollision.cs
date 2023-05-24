0f5b0b41954274a1ac76de97d54a53702df0158c
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-05-24 15:40:20 +0200

    Removed extra obstacle collision

--- Assets/Scripts/Gameplay/SpeederGround/ObstacleCollision.cs
+++ Assets/Scripts/Gameplay/SpeederGround/ObstacleCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleCollisionGround : MonoBehaviour
+public class ObstacleCollision : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
