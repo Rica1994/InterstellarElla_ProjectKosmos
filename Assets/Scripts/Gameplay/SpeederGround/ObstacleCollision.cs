4cb5dfa9fd3442cb39d887be9e01f4ddeab97c02
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-05-31 12:06:10 +0200

    Fixed player accidentally hitting other triggers and getting slowed

--- Assets/Scripts/Gameplay/SpeederGround/ObstacleCollision.cs
+++ Assets/Scripts/Gameplay/SpeederGround/ObstacleCollision.cs
@@ -21,7 +21,7 @@ public class ObstacleCollision : MonoBehaviour
         Assert.IsNotNull( _colliders, $"[{GetType()}] - colliders not found" );
     }
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out PlayerController player))
         {
