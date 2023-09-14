31d7abb008031e01ac880cf60dd5e9990290bc98
Koen <Konefu.Ponda@hotmail.com>
2023-09-14 20:13:17 +0200

    Mercurius blockout + mercurius scene name adjustments

--- Assets/Scripts/Player/SpeederSpace.cs
+++ Assets/Scripts/Player/SpeederSpace.cs
@@ -57,6 +57,21 @@ public class SpeederSpace : PlayerController
     [Header("Inversion of controls")]
     public bool InvertControls;
 
+    [Header("Magnet")]
+    [SerializeField]
+    private MagnetPlayerSpace _magnetSpace;
+
+    private bool _magnetIsActive;
+    private bool _magnetOnCooldown;
+
+    [SerializeField]
+    private float _magnetDuration = 3;
+    [SerializeField]
+    private float _magnetCooldownTime = 3;
+
+
+
+    #region Unity Functions
 
     private void Awake()
     {
@@ -71,38 +86,71 @@ public class SpeederSpace : PlayerController
         _moveComponent = new MoveComponent();
         _boostComponent = new MultiplierTimerComponent(0.0f, 1.5f, true, 2f, true, 1f);
     }
-
     void Start()
     {
         base.Start();
-        SetBounds();
+
+        _magnetSpace.EnableFullObject(false);
+
+        SetBounds();    
+    }
+    private void OnEnable()
+    {
+        // Subscribe to events
+        var playerInput = ServiceLocator.Instance.GetService<InputManager>().PlayerInput;
+        playerInput.Move.performed += OnMoveInput;
+        playerInput.Move.canceled += OnMoveInput;
+
+        playerInput.Action.performed += OnMagnetInput;
+    }
+    private void OnDisable()
+    {
+        if (_isApplicationQuitting)
+        {
+            return;
+        }
+
+        // Unsubscribe to events
+        var playerInput = ServiceLocator.Instance.GetService<InputManager>().PlayerInput;
+        playerInput.Move.performed -= OnMoveInput;
+        playerInput.Move.canceled -= OnMoveInput;
+
+        playerInput.Action.performed -= OnMagnetInput;
+    }
+    private void OnApplicationQuit()
+    {
+        _isApplicationQuitting = true;
     }
 
+    #endregion
+
+
+
+    #region Private Functions
+
+    private void OnMoveInput(InputAction.CallbackContext obj)
+    {
+        _input = obj.ReadValue<Vector2>();
+    }
+    private void OnMagnetInput(InputAction.CallbackContext obj)
+    {
+        if (_magnetIsActive == false && _magnetOnCooldown == false)
+        {
+            StartCoroutine(ActivateMagnet());
+        }
+    }
+    private void BoostEnded()
+    {
+        ServiceLocator.Instance.GetService<VirtualCameraManager>().ResetZoom();
+        _boostComponent.OnTimerEnded -= BoostEnded;
+    }
     private void SetBounds()
     {
         if (CinemachineCore.Instance.BrainCount > 0)
         {
-            //// Get Cinemachine information: dolly track camera offset
-            //CinemachineBrain cmBrain = CinemachineCore.Instance.GetActiveBrain(0);
-
-            //// get the virtual camera the brain is using (outdated)
-            //CinemachineVirtualCamera virtualCamera = cmBrain.ActiveVirtualCamera as CinemachineVirtualCamera;
-
-            //// virtual camera is null at start
-            //while(!virtualCamera)
-            //{
-            //    yield return null;
-            //    virtualCamera = cmBrain.ActiveVirtualCamera as CinemachineVirtualCamera;
-            //}
-
-            //var dollyCamera = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
-
-            //
             var dollyCamera = CameraWithDollyTrack.GetCinemachineComponent<CinemachineTrackedDolly>();
-            //
 
             Assert.IsNotNull(dollyCamera, $"[{GetType()}] - Virtual camera does not have a dolly track component");
-
 
             Vector3 offset = dollyCamera.m_PathOffset;
 
@@ -120,60 +168,6 @@ public class SpeederSpace : PlayerController
             Destroy(obj);
         }
     }
-
-    public override void UpdateController()
-    {
-        base.UpdateController();
-
-     //   _boostComponent.Update();
-
-        //UpdateCameraOffset();
-        //Debug.Log(_dollyCart.transform.forward);
-
-        Move();
-    }
-
-    private void UpdateCameraOffset()
-    {
-        // angleX and angleY don't make much sense anymore when both are being adjusted ... fu*k rotations
-
-        // get negative numbers instead of 0-360 range (this way i get the (-10)-(10) range)
-        float angleX = _dollyCart.transform.rotation.eulerAngles.x;
-        angleX = (angleX > 180) ? angleX - 360 : angleX;
-        float angleY = _dollyCart.transform.rotation.eulerAngles.y;
-        angleY = (angleY > 180) ? angleY - 360 : angleY;
-
-        // normalize / lerp the angles
-        float normalizedRotationX = Mathf.InverseLerp(-80f, 80f, angleX);
-        float offsetX = Mathf.Lerp(-2.5f, 2.5f, normalizedRotationX);
-
-        float normalizedRotationY = Mathf.InverseLerp(-35f, 35f, angleY);
-        float offsetY = Mathf.Lerp(-2f, 2f, normalizedRotationY);
-
-        Vector3 lerpedVector = new Vector3(offsetX, offsetY, -4.75f);
-      //  CameraFollowTransposer.m_FollowOffset = lerpedVector;
-    }
-
-    public void Boost(MultiplierTimerComponent boostComponent)
-    {
-        _boostComponent = boostComponent;
-        _boostComponent.Activate();
-        _boostComponent.OnTimerEnded += BoostEnded;
-        ServiceLocator.Instance.GetService<VirtualCameraManager>().ZoomOut();
-    }
-
-    private void BoostEnded()
-    {
-        ServiceLocator.Instance.GetService<VirtualCameraManager>().ResetZoom();
-        _boostComponent.OnTimerEnded -= BoostEnded;
-    }
-
-    public override void Collide(MultiplierTimerComponent knockbackComponent)
-    {
-        base.Collide(knockbackComponent);
-        ServiceLocator.Instance.GetService<VirtualCameraManager>().ResetZoom();
-    }
-
     private void CheckBounds()
     {
         if (InvertControls == false)
@@ -209,7 +203,6 @@ public class SpeederSpace : PlayerController
             }
         }
     }
-
     private void Move()
     {
         // Adjust input according to bounds
@@ -238,7 +231,6 @@ public class SpeederSpace : PlayerController
         transform.localRotation = Quaternion.identity;
         _characterController.enabled = true;
     }
-
     private void AnimateModel()
     {
         // Rotate towards Target
@@ -278,35 +270,80 @@ public class SpeederSpace : PlayerController
 
         _playerVisuals.transform.rotation = Quaternion.Lerp(_playerVisuals.transform.rotation, rot, 0.1f);
     }
-
-    private void OnEnable()
+    private void UpdateCameraOffset()
     {
-        // Subscribe to events
-        var playerInput = ServiceLocator.Instance.GetService<InputManager>().PlayerInput;
-        playerInput.Move.performed += OnMoveInput;
-        playerInput.Move.canceled += OnMoveInput;
+        // angleX and angleY don't make much sense anymore when both are being adjusted ... fu*k rotations
+
+        // get negative numbers instead of 0-360 range (this way i get the (-10)-(10) range)
+        float angleX = _dollyCart.transform.rotation.eulerAngles.x;
+        angleX = (angleX > 180) ? angleX - 360 : angleX;
+        float angleY = _dollyCart.transform.rotation.eulerAngles.y;
+        angleY = (angleY > 180) ? angleY - 360 : angleY;
+
+        // normalize / lerp the angles
+        float normalizedRotationX = Mathf.InverseLerp(-80f, 80f, angleX);
+        float offsetX = Mathf.Lerp(-2.5f, 2.5f, normalizedRotationX);
+
+        float normalizedRotationY = Mathf.InverseLerp(-35f, 35f, angleY);
+        float offsetY = Mathf.Lerp(-2f, 2f, normalizedRotationY);
+
+        Vector3 lerpedVector = new Vector3(offsetX, offsetY, -4.75f);
+        //  CameraFollowTransposer.m_FollowOffset = lerpedVector;
+    }
+    private IEnumerator ActivateMagnet()
+    {
+        _magnetIsActive = true;
+        _magnetOnCooldown = true;
+
+        // anable object and particles
+        _magnetSpace.EnableFullObject(true);
+        _magnetSpace.StartParticleSystem(true);
+
+        yield return new WaitForSeconds(_magnetDuration);
+
+        // disbale particles
+        _magnetSpace.StartParticleSystem(false);
+
+        yield return new WaitForSeconds(1f);
+
+        // disable the full object
+        _magnetSpace.EnableFullObject(false);
+        _magnetIsActive = false;
+
+        yield return new WaitForSeconds(_magnetCooldownTime);
+
+        // reset when cooldown reached
+        _magnetOnCooldown = false;
     }
 
-    private void OnDisable()
-    {
-        if (_isApplicationQuitting)
-        {
-            return;
-        }
+    #endregion
 
-        // Unsubscribe to events
-        var playerInput = ServiceLocator.Instance.GetService<InputManager>().PlayerInput;
-        playerInput.Move.performed -= OnMoveInput;
-        playerInput.Move.canceled -= OnMoveInput;
+
+    #region Public Functions
+
+    public override void UpdateController()
+    {
+        base.UpdateController();
+
+        //   _boostComponent.Update();
+
+        //UpdateCameraOffset();
+        //Debug.Log(_dollyCart.transform.forward);
+
+        Move();
+    }
+    public override void Collide(MultiplierTimerComponent knockbackComponent)
+    {
+        base.Collide(knockbackComponent);
+        ServiceLocator.Instance.GetService<VirtualCameraManager>().ResetZoom();
+    }
+    public void Boost(MultiplierTimerComponent boostComponent)
+    {
+        _boostComponent = boostComponent;
+        _boostComponent.Activate();
+        _boostComponent.OnTimerEnded += BoostEnded;
+        ServiceLocator.Instance.GetService<VirtualCameraManager>().ZoomOut();
     }
 
-    private void OnApplicationQuit()
-    {
-        _isApplicationQuitting = true;
-    }
-
-    private void OnMoveInput(InputAction.CallbackContext obj)
-    {
-        _input = obj.ReadValue<Vector2>();
-    }
+    #endregion
 }
