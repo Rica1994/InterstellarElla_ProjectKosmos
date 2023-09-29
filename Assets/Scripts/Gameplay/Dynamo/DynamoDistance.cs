8202a6ff9197770c6ce0e2a7611726f6b6915fc1
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-09-29 16:53:28 +0200

    Dynamo state update

--- Assets/Scripts/Gameplay/Dynamo/DynamoDistance.cs
+++ Assets/Scripts/Gameplay/Dynamo/DynamoDistance.cs
@@ -75,6 +75,7 @@ public class DynamoDistance : MonoBehaviour
         _dynamo = GetComponent<CinemachineDollyCart>();
         _dynamoDefaultSpeed = _dynamo.m_Speed;
         _defaultEnginePitch = _engineAudioSource.pitch;
+        DefaultState();
     }
 
 
