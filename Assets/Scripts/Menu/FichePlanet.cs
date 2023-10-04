ed0e639e93e646637c7cc4e591f03f970e1d2046
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-10-04 13:57:11 +0200

    the big fix

--- Assets/Scripts/Menu/FichePlanet.cs
+++ Assets/Scripts/Menu/FichePlanet.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameManager;
 
 public class FichePlanet : MonoBehaviour
 {
     public LevelType TypeLevel;
+    public Planet TypePlanet;
 }
