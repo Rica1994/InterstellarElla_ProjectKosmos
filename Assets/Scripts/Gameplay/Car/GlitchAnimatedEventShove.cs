32adcbdeb661f70962d8f92a9f0b2ff8b940909a
Koen <Konefu.Ponda@hotmail.com>
2023-10-13 00:37:59 +0200

    Pluto blocking out finalizing cameras, triggers, layout. Pluto Scene name changes. Pluto extra logic for glitch shove event. Pluto Material creation for setdressing.

--- Assets/Scripts/Gameplay/Car/GlitchAnimatedEventShove.cs
+++ Assets/Scripts/Gameplay/Car/GlitchAnimatedEventShove.cs
@@ -6,8 +6,51 @@ using UnityEngine;
 
 public class GlitchAnimatedEventShove : GlitchAnimatedEvent
 {
+    [Header("Swap-Cameras to enable/disable")]
+    [SerializeField]
+    private SwapCameraGlitch _swapCamToDisable;
+    [SerializeField]
+    private SwapCameraGlitch _swapCamToEnable;
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+        // disable/enable the cameras
+        if (_swapCamToEnable != null)
+        {
+            _swapCamToEnable.gameObject.SetActive(false);
+        }
+        if (_swapCamToDisable != null)
+        {
+            _swapCamToDisable.gameObject.SetActive(true);
+        }
+    }
+
     public override void ActivateCameraCutscene(SimpleCarController glitch)
     {
         base.ActivateCameraCutscene(glitch);
+
+        StartCoroutine(UseNewCameras());
+    }
+
+
+    private IEnumerator UseNewCameras()
+    {
+        // wait for the amount of time in the animation - 2f (still see jumpPad activate within cutscene)
+        yield return new WaitForSeconds(_animationCamera.clip.length + 0.1f);
+
+        // disabled one
+        if (_swapCamToDisable != null)
+        {
+            _swapCamToDisable.gameObject.SetActive(false);
+        }
+
+        // enable one
+        if (_swapCamToEnable != null)
+        {
+            _swapCamToEnable.gameObject.SetActive(true);
+        }
     }
 }
