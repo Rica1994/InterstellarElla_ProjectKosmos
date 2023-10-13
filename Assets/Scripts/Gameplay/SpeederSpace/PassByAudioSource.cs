53f79995f61ae9e11e39e1f39acb2e4f16da9126
beneffe <ben.gladine@fabriquefantastique.be>
2023-10-13 15:03:09 +0200

    Whoosh pass by

---
+++ Assets/Scripts/Gameplay/SpeederSpace/PassByAudioSource.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PassByAudioSource : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public void RecalculateAudioSourceDistances()
+    {
+        var parent = transform.parent;
+
+        if (parent != null && parent.localScale.sqrMagnitude > 1.1f && audioSource != null)
+        {
+            audioSource.minDistance *= parent.localScale.magnitude;
+            audioSource.maxDistance *= parent.localScale.magnitude;
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource.spatialBlend == 1.0f) // Check if AudioSource is set to 3D
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, audioSource.minDistance);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, audioSource.maxDistance);
+        }
+    }
+#endif
+
+}
