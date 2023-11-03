caa793e0f0f634060d936c236ae0409dfb76d2d4
beneffe <ben.gladine@fabriquefantastique.be>
2023-11-03 15:53:36 +0100

    Soundtrack chainaction adapted for fadeout current

--- Assets/Scripts/Framework/ChainAction/CustomActions/SoundtrackChainAction.cs
+++ Assets/Scripts/Framework/ChainAction/CustomActions/SoundtrackChainAction.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 
 public class SoundtrackChainAction : ChainAction
 {
+    public enum SoundAction
+    {
+        None = 0,
+        FadeOutCurrentSoundtrack = 1,
+    }
+
     [SerializeField]
     private AudioClip _audioClip;
 
     [SerializeField]
     private bool _fadeIn = false;
+
+    [SerializeField]
+    private bool _fadeOut = false;
 
     [SerializeField]
     private bool _crossfade = false;
@@ -23,10 +32,25 @@ public class SoundtrackChainAction : ChainAction
     [SerializeField]
     private float _targetVolume = 1.0f;
 
+    [SerializeField]
+    private SoundAction _soundAction = SoundAction.None;
+
+    [SerializeField]
+    private float _fadeTime = 1.0f;
+
     protected override void Awake()
     {
         base.Awake();
-
+        if (_soundAction == SoundAction.FadeOutCurrentSoundtrack) 
+        {
+            _maxTime = _fadeTime;
+            return;
+        } 
+        else if (_audioClip == null)
+        {
+            Debug.LogError("Audio clip not filled for " + gameObject.name);
+            return;
+        }
         _maxTime = _audioClip.length;
     }
 
@@ -34,7 +58,14 @@ public class SoundtrackChainAction : ChainAction
     {
         base.Execute();
         var soundTrackManager = ServiceLocator.Instance.GetService<SoundtrackManager>();
-
-        soundTrackManager.PlayClip(_audioClip, _fadeIn, _crossfade, _loop, _targetVolume, _letCurrentClipFinish);
+        switch (_soundAction)
+        {
+            case SoundAction.FadeOutCurrentSoundtrack:
+                soundTrackManager.FadeOutCurrent(_fadeTime);
+                break;
+            default:
+                soundTrackManager.PlayClip(_audioClip, _fadeIn, _crossfade, _loop, _targetVolume, _letCurrentClipFinish);
+                break;
+        }
     }
 }
