883896035a50f9b880539a946857914f3879da97
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-11-05 20:16:04 +0100

    Ella Blinking and other polish

--- Assets/Scripts/StateMachineBehaviourScripts/RandomLoop.cs
+++ Assets/Scripts/StateMachineBehaviourScripts/RandomLoop.cs
@@ -10,40 +10,29 @@ public class RandomLoop : StateMachineBehaviour
     [SerializeField]
     private float _maxSeconds = 4.0f;
 
+    [SerializeField]
+    private List<string> _additionalTriggers = new List<string>();
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
    {
-        ServiceLocator.Instance.GetService<AudioController>().StartCoroutine(RandomTime(animator, stateInfo));
-    }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
+        if (_additionalTriggers.Count > 0 && Random.Range(0, 5) == 0)
+        {
+            animator.SetTrigger(_additionalTriggers[Random.Range(0, _additionalTriggers.Count)]);
+        }
+        else
+        {
+            ServiceLocator.Instance.GetService<AudioController>().StartCoroutine(RandomTime(animator, stateInfo));
+        }
+   }
 
     IEnumerator RandomTime(Animator animator, AnimatorStateInfo stateInfo)
     {
         float randomSeconds = Random.Range(_minSeconds, _maxSeconds);
         yield return new WaitForSeconds(randomSeconds);
-        animator.Play(stateInfo.shortNameHash, 1, 0);
+        if (animator != null)
+        {
+            animator.Play(stateInfo.shortNameHash, 1, 0);
+        }
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
