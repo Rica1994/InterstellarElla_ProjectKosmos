c77007e0a10a540984966696e12263b64587c8ee
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-11-12 23:04:30 +0100

    Update Mercury lighting and materials

---
+++ Assets/Scripts/Framework/ChainAction/CustomActions/CinemachineBlendAction.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityCore.Audio;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CinemachineBlendAction : ChainAction
+{
+    [SerializeField]
+    private CinemachineBrain _cinemachineBrain;
+    [SerializeField]
+    private CinemachineBlendDefinition _cinemachineBlendDefinition;
+
+    public override void Execute()
+    {
+        base.Execute();
+        if (_cinemachineBrain != null)
+        {
+            _cinemachineBrain.m_DefaultBlend = _cinemachineBlendDefinition;
+        }
+    }
+}
