66d23e3bc7b75223988c3a8af1d482d5ad7be18a
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-11-24 17:46:22 +0100

    speeder ground chain actions

---
+++ Assets/Scripts/Framework/ChainAction/CustomActions/CinemachineUpdateChainAction.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CinemachineUpdateChainAction : ChainAction
+{
+    [SerializeField]
+    private CinemachineBrain _cinemachineBrain;
+
+    [SerializeField]
+    private CinemachineBrain.UpdateMethod _updateMethod;
+
+    public override void Execute()
+    {
+        base.Execute();
+        if (_cinemachineBrain != null)
+        {
+            _cinemachineBrain.m_UpdateMethod = _updateMethod;
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/ChainAction/CustomActions/SpeederGroundSpeedChainAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeederGroundSpeedChainAction : ChainAction
+{
+    [SerializeField]
+    private float _forwardSpeed;
+    [SerializeField]
+    private SpeederGround _speederGround;
+
+    public override void Execute()
+    {
+        base.Execute();
+        _speederGround.speedForward = _forwardSpeed;
+    }
+}
