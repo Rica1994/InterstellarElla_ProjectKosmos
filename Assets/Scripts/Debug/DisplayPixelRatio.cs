c1dee3fa1a2fa6b4485258bd0c3d772b6c31c0b2
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-12-04 12:29:28 +0100

    Update startup scene and camera setup

---
+++ Assets/Scripts/Debug/DisplayPixelRatio.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPixelRatio : MonoBehaviour
+{
+    public Text pixelRatioText; // Assign this in the inspector
+
+    // Method to be called from JavaScript
+    public void UpdatePixelRatio(string pixelRatioString)
+    {
+        float pixelRatio = float.Parse(pixelRatioString);
+        pixelRatioText.text = "Pixel Ratio: " + pixelRatio;
+    }
+}
