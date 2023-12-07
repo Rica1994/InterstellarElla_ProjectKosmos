6e4fbd22f34eec6c18f44f70c25b5e04d8d778a1
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2023-12-07 16:32:21 +0100

    Client or dev enabler + implementation in intros/outros

---
+++ Assets/Scripts/Debug/ClientOrDevEnabler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-999)]
+public class ClientOrDevEnabler : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager.BuildType _buildType;
+
+    private void Awake()
+    {
+        if (ServiceLocator.Instance.GetService<GameManager>().TargetBuildType != _buildType)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
